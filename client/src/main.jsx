5b9fe38e14f749ff7881538cd4b8b74e89a28553
Roesnware <bubwub04@gmail.com>
2024-08-05 14:46:14 -0400

    more cleaning out/setting up main pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import DOM and router 
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// import pages 
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Account from './pages/Account.jsx';
+
+// define router paths
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Account />
+      }
+    ]
+  },
+]);
+
+// mount router
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
